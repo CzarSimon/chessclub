b35b3801cb80535c58a2257ec1c19a667b5f2e30
Simon Lindgren <simon.g.lindgren@gmail.com>
2021-06-19 15:19:06 +0200

    Added baseline of game-server data model

---
+++ game-server/resources/db/sqlite/1__baseline.sql
@@ -0,0 +1,51 @@
+-- +migrate Up
+CREATE TABLE `game` (
+    `id` VARCHAR(50) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`)
+);
+CREATE TABLE `color` (
+    `name` VARCHAR(5) NOT NULL,
+    PRIMARY KEY (`name`)
+);
+CREATE TABLE `player` (
+    `id` VARCHAR(50) NOT NULL,
+    `game_id` VARCHAR(50) NOT NULL,
+    `user_id` VARCHAR(50) NOT NULL,
+    `color` VARCHAR(5) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+    FOREIGN KEY (`color`) REFERENCES `color` (`name`)
+);
+CREATE TABLE `ply` (
+    `id` VARCHAR(50) NOT NULL,
+    `turn` INTEGER NOT NULL,
+    `move` VARCHAR(10) NOT NULL,
+    `game_id` VARCHAR(50) NOT NULL,
+    `player_id` VARCHAR(50) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE(`turn`, `player_id`),
+    FOREIGN KEY (`game_id`) REFERENCES `game` (`id`),
+    FOREIGN KEY (`player_id`) REFERENCES `player` (`id`),
+);
+CREATE TABLE `comment` (
+    `id` VARCHAR(50) NOT NULL,
+    `text` INTEGER NOT NULL,
+    `ply_id` VARCHAR(10) NOT NULL,
+    `user_id` VARCHAR(50) NOT NULL,
+    `created_at` DATETIME NOT NULL,
+    `updated_at` DATETIME NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`ply_id`) REFERENCES `ply` (`id`)
+);
+INSERT INTO `color`(`name`)
+VALUES ('WHITE', 'BLACK');
+-- +migrate Down
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `ply`;
+DROP TABLE IF EXISTS `player`;
+DROP TABLE IF EXISTS `color`;
+DROP TABLE IF EXISTS `game`;
